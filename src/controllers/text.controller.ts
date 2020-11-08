22fc9543b11e41c0aac2892226bcbfaf5303d26c
Ivan Requena Garcia <ivanreq97@gmail.com>
2020-11-08 19:00:42 +0100

    ñeh

--- src/controllers/text.controller.ts
+++ src/controllers/text.controller.ts
@@ -1,23 +1,32 @@
 import { Request, Response } from 'express';
-import crypto, { Cipher } from 'crypto';
+import crypto from 'crypto';
+var CryptoJS = require("crypto-js")
 
 import server from '../index'
 
-const secret = 'bobito';
+/* const secret = 'bobito';
 const pswd = 'SCCBD';
 const algorithm = 'aes-128-cbc';
 const keyAndIv = crypto.pbkdf2Sync(secret, pswd, 1024, 32, 'sha1');
 const key = keyAndIv.slice(0, 16);
-const iv = keyAndIv.slice(16, 32);
+const iv = keyAndIv.slice(16, 32); */
 
-let mensaje : string;
+const secretKey = CryptoJS.enc.Hex.parse("4b173f3b3c2366674695d1a17a04752a");
+
+var mensaje : string;
+
+console.log('key:', secretKey);
 
 class TextController {
 
     public async postText (req:Request, res:Response){
         try{
-            console.log('Petición POST realizada! Mensaje cifrado:', req.body.text);
-            mensaje = decrypt(key,iv,req.body.text);
+            console.log("BODY:"+req.body.text.cipherText);
+            console.log('Petición POST realizada! Mensaje cifrado:', req.body.text.cipherText);
+            let iv = req.body.text.iv;
+            let mensaje = req.body.text.cipherText;
+            mensaje = decrypt(secretKey,iv,mensaje);
+            console.log("HOLI: " + mensaje);
             res.status(200).json({"text": mensaje});
         }
         catch{
@@ -29,8 +38,13 @@ class TextController {
         try{
             if(mensaje==null) mensaje = "Mensaje por defecto";
             console.log('Petición GET realizada');
-            let datacipher = encrypt(key,iv,mensaje);
-            res.status(200).json(datacipher);
+            var iv = CryptoJS.lib.WordArray.random(128/8);
+            let datacipher = encrypt(secretKey,iv,mensaje);
+            let data = {
+                dataCipher: datacipher,
+                iv: iv
+            }
+            res.status(200).json(data);
         }
         catch{
             res.status(500).json({"text": 'Internal Server Error'});
@@ -38,8 +52,8 @@ class TextController {
     }
 }
 
-function encrypt(key:Buffer,iv:Buffer,mensaje:string){
-    let cipher = crypto.createCipheriv(algorithm,key,iv);
+function encrypt(key:string,iv:Buffer,mensaje:string){
+    let cipher = crypto.createCipheriv('aes-128-cbc',key,iv);
     let encrypted = cipher.update(mensaje,'utf8','hex');
     encrypted += cipher.final('hex');
     console.log('Mensaje cifrado:',encrypted);
@@ -47,7 +61,7 @@ function encrypt(key:Buffer,iv:Buffer,mensaje:string){
 }
 
 function decrypt(key:Buffer,iv:Buffer,cipherdata:string){
-    let decipher = crypto.createDecipheriv(algorithm,key,iv);
+    let decipher = crypto.createDecipheriv('aes-128-cbc',key,iv);
     let decrypted = decipher.update(cipherdata,'hex','utf8');
     decrypted+=decipher.final('utf-8');
     console.log('Mensaje descifrado:',decrypted);
